0d33ef1a4ea54fd870187c756c284ad04bb7fd33
Igornovic21 <kamguefokoigor@gmail.com>
2026-07-03 13:47:28 -0400

    feat: integrate Datafaker for automatic user generation in development

--- services/xflow-auth-service/src/main/java/auth/service/xflow_auth_service/XflowAuthServiceApplication.java
+++ services/xflow-auth-service/src/main/java/auth/service/xflow_auth_service/XflowAuthServiceApplication.java
@@ -2,15 +2,10 @@ package auth.service.xflow_auth_service;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
-import auth.service.xflow_auth_service.models.User;
-import auth.service.xflow_auth_service.models.enums.UserRole;
-import auth.service.xflow_auth_service.repositories.UserRepository;
 
 import java.time.OffsetDateTime;
 
@@ -21,29 +16,7 @@ import java.time.OffsetDateTime;
 public class XflowAuthServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("🚀 XFlow Auth Service launched !!!");
 		SpringApplication.run(XflowAuthServiceApplication.class, args);
-	}
-	
-	@Bean
-	CommandLineRunner initDatabase(UserRepository repository, PasswordEncoder encoder) {
-		return args -> {
-			System.out.println("🚀 XFlow Auth Service launched !!!");
-			if (repository.findByEmail("admin@xflow.io").isEmpty()) {
-				User testUser = new User();
-				testUser.setEmail("admin@xflow.io");
-				testUser.setPassword(encoder.encode("password123"));
-				testUser.setRole(UserRole.ROLE_ADMIN);
-				testUser.setActive(true);
-				repository.save(testUser);
-			}
-			if (repository.findByEmail("operator@xflow.io").isEmpty()) {
-				User testUser = new User();
-				testUser.setEmail("operator@xflow.io");
-				testUser.setPassword(encoder.encode("password123"));
-				testUser.setPin(encoder.encode("123456"));
-				testUser.setRole(UserRole.ROLE_OPERATOR);
-				repository.save(testUser);
-			}
-		};
 	}
 }
